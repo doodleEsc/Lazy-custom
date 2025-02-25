1c6f48aa08ca4ea327fba0932843d2cdef35ee35
范立洲 <fanlizhou@yunqilaohe.com>
2025-02-25 11:55:37 +0800

    fix: add function call

--- lua/lazycustom/init.lua
+++ lua/lazycustom/init.lua
@@ -14,12 +14,20 @@ M.setup = function(opts)
     M.options = vim.tbl_deep_extend("force", {}, default, opts or {})
     -- Iterate over M.options.opts and set vim options
     for key, value in pairs(M.options.opts) do
-        vim.o[key] = value
+        if type(value) == "function" then
+            pcall(value)
+        else
+            vim.o[key] = value
+        end
     end
 
     -- Iterate over M.options.globals and set vim g
     for key, value in pairs(M.options.globals) do
-        vim.g[key] = value
+        if type(value) == "function" then
+            pcall(value)
+        else
+            vim.g[key] = value
+        end
     end
 end
 
